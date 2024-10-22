7e489e65bc446a8fa6d366ab60ad4cb439e8fe72
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-22 12:50:05 +0000

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,11 +9,13 @@
 */
 	int main(void)
 {
-	int n, l;
+	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 	l = (n % 10 + 10) % 10;
-	printf("Last digit of %d is %d and is %s\n", n, l,
+	printf("Last digit of n - 6 is 0  and is 5\n", n, l,
 	l > 5 ? "greater than 5" : l ? "less than 6 and not 0" : "0");
 	return (0);
 }
