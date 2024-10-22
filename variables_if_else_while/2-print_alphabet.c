e0b83e8f46e6b426167191115b54779b5ac2eaf4
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-22 14:20:49 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,23 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/**
+ * main - Prints the alphabet in uppercase
+ *
+ * Description: This program prints the alphabet in uppercase,
+ * followed by a new line. Only putchar can be used.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	putchar(c);
-	}
-	putchar('\n');
+char c;
 
-	return (0);
+for (c = 'a' ; c <= 'z' ; c++)
+{
+putchar(c);
 }
+
+putchar('\n');
+
+return (0);
