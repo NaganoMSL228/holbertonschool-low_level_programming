4c6877950eb5f44634a2439a7b1997497b7335f1
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-22 13:17:17 +0000

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,14 +24,15 @@
 
 	if (l > 6)
 	{
-	printf("and is greater than 5\n")
+	printf("and is greater than 5\n");
 	}
 	else if (l == 0)
 	{
-	printf("and is 0\n")
+	printf("and is 0\n");
 	}
 	else
-	printf{"and is less than 6 and not 0\n"}
-
+	{
+	printf("and is less than 6 and not 0\n");
+	}
 	return (0);
 }
