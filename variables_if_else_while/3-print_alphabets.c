94fa0c7537ea4275484f1bc8b40a22f5c15794c3
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-23 12:50:48 +0000

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -4,16 +4,18 @@
 *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char letter;
-	for(letter = 'a'; letter <= 'z'; letter++)
-	putchar(letter);
-	
+	char (letter);
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter)
+
 	for (letter = 'A'; letter <= 'Z'; letter++)
-	putchar(letter);
-	
+		putchar(letter);
+
 	putchar('\n');
-	
+
 	return (0);
 }
