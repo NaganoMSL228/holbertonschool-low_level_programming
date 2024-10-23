0d0850e99bb21cb063d295ffba0b3d6d3cf0b013
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-23 14:18:24 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the alphabet in uppercase
+ *
+ * Description: This program prints the alphabet in uppercase,
+ * followed by a new line. Only putchar can be used.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%i", i);
+	printf("\n");
+return (0);
+}
