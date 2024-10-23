e21fdde7e17bbc8e5952f65acfa8855e0827e716
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-23 17:37:27 +0000

    8-print_base16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -11,13 +11,9 @@ int main(void)
 	int n;
 
 	for (n = 0; n < 10; n++)
-	{
-		putchar(n);
-	}
+		putchar(n + '0');
 	for (n = 0; n < 6; n++)
-	{
-		putchar(n);
-	}
+		putchar(n + 'a');
 	putchar('\n');
 	return (0);
 }
