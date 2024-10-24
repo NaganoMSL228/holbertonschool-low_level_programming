52d1e10e38b37a51024e7eb62bcddedd28886c0b
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-24 11:12:22 +0000

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet -  Prints the alphabet in uppercase
+ *
+ * Description: This program prints the alphabet in lowercase,
+ * followed by a new line. Only _putchar can be used.
+ * Return: Always 10 (Success)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
