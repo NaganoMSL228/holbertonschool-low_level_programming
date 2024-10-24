548eca0d1272ca4f2a51e3678c7bf3e44fd45893
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-24 08:39:34 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
+#include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char str[] = "_putchar\n";
+        _putchar("_");
+        _putchar("p");
+        _putchar("u");
+        _putchar("t");
+        _putchar("c");
+        _putchar("h");
+        _putchar("a");
+        _putchar("r");
 
-	write(1, str, 9);
-	return (0);
+        _putchar("\n");
+
+return (0);
 }
