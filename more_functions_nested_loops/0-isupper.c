e2bc8e632b720bf684b1e8e517237365240458d2
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-28 10:25:00 +0000

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isdigit - Checks for a digit (0 through 9)
+ * _isupper - Checks if a character is uppercase
  * @c: The character to be checked
  *
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isdigit(int c)
+int _isupper(int c)
 {
-    if (c >= '0' && c <= '9')
+    if (c >= 'A' && c <= 'Z')
         return 1;
     else
         return 0;
