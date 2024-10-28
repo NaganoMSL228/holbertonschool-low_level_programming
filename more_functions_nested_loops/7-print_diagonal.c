4ef7b931bd6bfb755773beec749817cdaf0342de
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-28 19:38:51 +0000

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,7 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	{
-		_putchar(\n);
+		_putchar('\n');
 	}
 	else
 	{
@@ -20,6 +20,7 @@ void print_diagonal(int n)
 			{
 				_putchar(' ');
 			}
+			_putchar(\\);
 			_putchar('\n');
 		}
 	}
