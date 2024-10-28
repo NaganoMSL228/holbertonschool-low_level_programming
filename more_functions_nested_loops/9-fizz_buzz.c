259cca4dd67a3b1d345304eb259a2c1926fcfb34
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-28 21:13:29 +0000

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,15 @@ int main(void)
 	{
 		if (i % 3 = 0 && i % 5 = 0)
 		{
-			printf('FizzBuzz');
+			printf("FizzBuzz");
 		}
 		else if (i % 3 = 0)
 		{
-			printf('Fizz');
+			printf("Fizz");
 		}
 		else if (i % 5 = 0)
 		{
-			printf('Buzz');
+			printf("Buzz");
 		}
 		else
 		{
@@ -30,10 +30,10 @@ int main(void)
 
 		if (i < 100)
 		{
-			printf(' ');
+			printf(" ");
 		}
 	}
-	printf('\n');
+	printf("\n");
 
 	return (0);
 }
