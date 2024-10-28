7dfaf344f1084cd65d7407c26519d29090aa06ce
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-28 10:23:10 +0000

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-/* Déclarations de fonctions */
-int _putchar(char c);
-int _isupper(int c);
-/* ... autres déclarations ... */
-void print_square(int size);
-
-#endif /* MAIN_H */
+/**
+ * _isdigit - Checks for a digit (0 through 9)
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+    if (c >= '0' && c <= '9')
+        return 1;
+    else
+        return 0;
+}
