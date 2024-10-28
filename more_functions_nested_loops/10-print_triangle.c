30cfa30c22b2c04a36b924e48842c574d356a308
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-28 21:25:59 +0000

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -5,14 +5,21 @@
  * @size: size of the triangle
  */
 void print_triangle(int size)
-{
-    int i, j;
+#include "main.h"
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-            _putchar(j < size - i - 1 && ' ' : '#');
-        i < size - 1 &&  _putchar('\n') : 0;
-    }
-    _putchar('\n');
+/**
+ * print_triangle - prints a triangle of a given size
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar(j < size - i - 1 && ' ' : '#');
+		i < size - 1 && _putchar('\n') : 0;
+	}
+	_putchar('\n');
 }
