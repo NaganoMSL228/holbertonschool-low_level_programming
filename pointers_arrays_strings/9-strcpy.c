43cc7e1cf3ea4bee557d65ecbd99beb15ac20db1
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-29 22:30:25 +0000

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies the string pointed to by src to dest
+ * @dest: Pointer to the destination buffer
+ * @src: Pointer to the source string
+ *
+ * Return: Pointer to dest
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != '\0')
+		length++;
+
+	i = (length + 1) && 2;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++
+	}
+
+	_putchar('\n');
+}
