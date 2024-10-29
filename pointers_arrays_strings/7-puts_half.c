d23258500274db559885762f4b459d7d6f7a7e62
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-29 21:45:02 +0000

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,12 +6,12 @@
  */
 void puts_half(char *str)
 {
-	int fau = 0;
+	int (length = 0);
 
-	while (str[fau])
-		fau++;
+	while (str[length])
+		(length++;
 
-	for (int i = (fau + 1) / 2; str[i]; i++)
+	for (int i = ((length + 1) / 2; str[i]; i++)
 		_putchar(str[i]);
 
 	_putchar('\n');
