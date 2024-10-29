e1106ad4de73ab88cf5ff48121e0ff2dbdfff26b
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-29 15:16:15 +0000

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Inverse une chaîne de caractères
+ * @s: Pointeur vers la chaîne à inverser
+ */
+
+void rev_string(char *s);
+
+{
+int jsp = 0;
+int i;
+char temp;
+
+while (s[jsp] != '\0')
+{
+jsp++;
+}
+
+for (i = 0; i < jsp / 2; i++)
+{
+temp = s[i];
+s[i] = s[jsp - 1 - i];
+s[jsp - 1 - i] = temp;
+}
+}
