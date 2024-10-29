5fe71433faeed3b02c3ac3c347856ce18f1c86eb
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-29 21:37:37 +0000

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -4,19 +4,14 @@
  * puts_half - prints half of a string, followed by a new line
  * @str: string to be printed
  */
-void puts_half(char *str);
+void puts_half(char *str)
 {
-int fautjedors = 0, i, n
+int fautjedors = 0
 
-
-while (str[fautjedors] != '\0')
+while (str(fautjedors))
 fautjedors++;
 
-
-n = (fautjedors - 1) / 2;
-
-
-for (i = fautjedors - n; i < fautjedors; i++)
+for (int i = (fautjedors + 1) / 2; str[i]; i++)
 _putchar(str[i]);
 
 _putchar('\n');
