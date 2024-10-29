2dbb073368e7ad61de538582f2ca22fd1a7a7dac
NaganoMSL228 <9794@holbertonstudents.com>
2024-10-29 13:24:28 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Retourne la longueur d'une chaîne de caractères
+ * @s: Pointeur vers la chaîne de caractères
+ *
+ * Return: La longueur de la chaîne
+ */
+int _strlen(char *s)
+{
+int nur = 0;
+
+while (*s != '\0')
+{
+nur++;
+s++;
+}
+
+return nur;
+}
