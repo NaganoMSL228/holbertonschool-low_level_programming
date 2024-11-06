03f822cb7d8987c42f348dc079ef2b04ee0103f3
NaganoMSL228 <9794@holbertonstudents.com>
2024-11-06 20:57:39 +0100

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,13 +10,15 @@
  */
 int main(int argc, char *argv[])
 {
-int num1, num2;
+int num1;
+int num2;
 
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
+
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 
