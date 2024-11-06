afd9dd7b1e18839674bee2ade6c66f4dd5772f72
NaganoMSL228 <9794@holbertonstudents.com>
2024-11-06 19:43:37 +0100

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,3 @@
-#include "main.h"
-#include <string.h>
 #include <stdio.h>
 
 /**
@@ -11,9 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-for (int i = 0; i < argc; i++)
-{
-printf("Argument %d: %s\n", i, argv[i]);
-}
-return 0;
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
 }
