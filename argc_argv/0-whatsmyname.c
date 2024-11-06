4945d891516d79219aa7b877a35793760dacd73a
NaganoMSL228 <9794@holbertonstudents.com>
2024-11-06 19:20:13 +0100

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-printf("%s\n", argv[0]);
+char *program_name = argv[0];
+
+
+char *last_slash = strrchr(program_name, '/');
+
+if (last_slash != NULL) {
+
+printf("%s\n", last_slash + 1);
+} else
+{
+
+printf("%s\n", program_name);
+}
+
 return 0;
 }
