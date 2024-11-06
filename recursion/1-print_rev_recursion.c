38ae9ebf76addd49930bf2c2bdd982e6257f992e
NaganoMSL228 <9794@holbertonstudents.com>
2024-11-06 14:07:01 +0100

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Imprime une chaîne de caractères à l'envers
- * @s: La chaîne de caractères à imprimer
+ * _print_rev_recursion - Imprime une chaine de caracteres a l'envers
+ * @s: La chaine de caracteres a imprimer
  */
 void _print_rev_recursion(char *s)
 {
