9613fe1c2e8571ff1d44777652402b4330424f8e
NaganoMSL228 <9794@holbertonstudents.com>
2024-11-06 19:23:08 +0100

    0-whatsmyname.c

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -5,14 +5,11 @@
 #include <ctype.h>
 #include <unistd.h>
 
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _putchar(char c);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
+void _putchar(char c);
+void print_numbers(void);
+void print_most_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
 
 #endif
