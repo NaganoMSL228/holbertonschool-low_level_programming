a4e23e56cd35ecda77eb0854b1b5b003be0691dd
NaganoMSL228 <9794@holbertonstudents.com>
2024-11-06 15:32:31 +0100

    7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -4,20 +4,18 @@
  * print_chessboard - Prints the chessboard
  * @a: 2D array representing the chessboard
  *
- * Description: This function prints an 8x8 chessboard
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
-	int row, col;
+int row, col;
 
-	for (row = 0; row < 8; row++)
-	{
-		for (col = 0; col < 8; col++)
-		{
-			_putchar(a[row][col]);
-			if (col < 7)
-				_putchar(' ');
-		}
-		_putchar('\n');
-	}
+for (row = 0; row < 8; row++)
+{
+for (col = 0; col < 8; col++)
+{
+_putchar(a[row][col]);
+}
+_putchar('\n');
+}
 }
