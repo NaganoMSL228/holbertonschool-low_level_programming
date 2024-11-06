7508ae62b4650143ae4e0cfa019afd1d3a92d87a
NaganoMSL228 <9794@holbertonstudents.com>
2024-11-06 19:40:56 +0100

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * main - Prints the name of the program followed by its arguments
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 for (int i = 0; i < argc; i++)
