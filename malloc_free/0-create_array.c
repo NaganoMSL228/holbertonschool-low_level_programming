8020e98b0184f598911e23dd3c35059e3c399ea4
NaganoMSL228 <9794@holbertonstudents.com>
2024-11-12 21:00:47 +0100

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * create_array - Creates an array of chars and initializes it with a specific char
+ * create_array - Creates an array of chars and initializes it with a specific
  * @size: The size of the array to be created
  * @c: The char to initialize the array with
  *
