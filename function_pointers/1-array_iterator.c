bdbc26cab80e20e2a76df5c19228dad7b459db65
NaganoMSL228 <9794@holbertonstudents.com>
2024-11-18 12:22:31 +0100

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,22 +1,23 @@
 #include <stddef.h>
-#include "function_pointers.h"
 
 /**
  * array_iterator - Executes a function on each element of an array
  * @array: Pointer to the integer array
  * @size: Size of the array
- * @action: Pointer to the function to be executed on each array element
+ * @action: Function pointer to the action to be performed on each element
  *
- * Description: Iterates through the array and applies the given function
- * to each element. Checks for NULL pointers before execution.
+ * Description: This function applies the given action function to each
+ * element of the array.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-if (array == NULL || action == NULL)
-return;
-for (size_t i = 0; i < count; i++)
- (size_t i = 0; i < size; i++)
+size_t i;
+
+if (array && action)
+{
+for (i = 0; i < size; i++)
 {
 action(array[i]);
+}
 }
 }
