befdfd0d04d839c2abd0ae3765e98e9f2007623f
NaganoMSL228 <9794@holbertonstudents.com>
2024-11-19 09:26:53 +0100

    0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,7 +9,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 va_list args;
-int barca = 0;
+int sum = 0;
 unsigned int i;
 
 if (n == 0)
@@ -18,9 +18,9 @@ return (0);
 va_start(args, n);
 
 for (i = 0; i < n; i++)
-barca += va_arg(args, int);
+sum += va_arg(args, int);
 
 va_end(args);
 
-return (barca);
+return (sum);
 }
