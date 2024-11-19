48c78568099ae5a4c8c633d96beef53361c6bbce
NaganoMSL228 <9794@holbertonstudents.com>
2024-11-19 09:24:55 +0100

    0-sum_them_all.c

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Calcule la somme de tous les paramètres
+ * @n: Nombre de paramètres
+ *
+ * Return: Somme de tous les paramètres, ou 0 si n == 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list args;
+int barca = 0;
+unsigned int i;
+
+if (n == 0)
+return (0);
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+barca += va_arg(args, int);
+
+va_end(args);
+
+return (barca);
+}
