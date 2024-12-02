1721e86254917db82e1fa50d5790d4602c51ae5d
NaganoMSL228 <9794@holbertonstudents.com>
2024-12-02 19:16:26 +0100

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -8,63 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t node_count = 0;
-char *str;
-unsigned int len;
+	size_t count = 0;
 
-while (h != NULL)
-{
-str = h->str;
-len = h->len;
+	while (h)
+	{
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 
-_putchar('[');
-if (str == NULL)
-{
-_putchar('0');
-_putchar(']');
-_putchar(' ');
-_putchar('(');
-_putchar('n');
-_putchar('i');
-_putchar('l');
-_putchar(')');
-}
-else
-{
-print_number(len);
-_putchar(']');
-_putchar(' ');
-print_string(str);
-}
-_putchar('\n');
+		count++;
+		h = h->next;
+	}
 
-h = h->next;
-node_count++;
-}
-
-return (node_count);
-}
-
-/**
- * print_number - prints an unsigned integer
- * @n: number to print
- */
-void print_number(unsigned int n)
-{
-if (n / 10)
-print_number(n / 10);
-_putchar((n % 10) + '0');
-}
-
-/**
- * print_string - prints a string
- * @s: string to print
- */
-void print_string(char *s)
-{
-while (*s)
-{
-_putchar(*s);
-s++;
-}
+	return (count);
 }
