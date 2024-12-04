7dd0a3e943389b4239ba2f7b06a44fbaefdd0903
NaganoMSL228 <9794@holbertonstudents.com>
2024-12-05 00:23:10 +0100

    Change the for 16 pts

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,71 +2,47 @@
 #include <stdio.h>
 
 /**
-* error_exit - Print an error message and exit with a given code.
-* @code: Exit code.
-* @message: Error message format.
-* @filename: Name of the file causing the error.
-*/
-void error_exit(int code, const char *message, const char *filename)
-{
-	dprintf(STDERR_FILENO, message, filename);
-	exit(code);
-}
-
-/**
-* copy_file - Copies the content from one file to another.
-* @file_from: The source file.
-* @file_to: The destination file.
+* read_textfile - Reads a text file and prints it to POSIX standard output.
+* @filename: The name of the file to read.
+* @letters: The number of letters to read and print.
 *
-* Return: 1 on success, or -1 on failure.
+* Return: The actual number of letters it could read and print, or 0 on failure
 */
-int copy_file(const char *file_from, const char *file_to)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd_from, fd_to;
+	int fd;
 
 	ssize_t bytes_read, bytes_written;
-	char buffer[1024];
+	char *buffer;
 
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", file_from);
+	if (filename == NULL)
+		return (0);
 
-	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664); // rw-rw-r--
-	if (fd_to == -1)
-		error_exit(99, "Error: Can't write to %s\n", file_to);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-			error_exit(99, "Error: Can't write to %s\n", file_to);
+		free(buffer);
+		return (0);
 	}
 
+	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
-		error_exit(98, "Error: Can't read from file %s\n", file_from);
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-	if (close(fd_from) == -1)
-		error_exit(100, "Error: Can't close fd %d\n", fd_from);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	free(buffer);
+	close(fd);
 
-	if (close(fd_to) == -1)
-		error_exit(100, "Error: Can't close fd %d\n", fd_to);
+	if (bytes_written != bytes_read)
+		return (0);
 
-	return (1);
-}
-
-/**
-* main - Entry point of the program.
-* @argc: Argument count.
-* @argv: Argument vector.
-*
-* Return: Always 0 on success.
-*/
-int main(int argc, char **argv)
-{
-	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to\n", "");
-
-	copy_file(argv[1], argv[2]);
-
-	return (0);
+	return (bytes_written);
 }
