bd7bcc4e72bd8415028f464a20df8c8a18237568
NaganoMSL228 <9794@holbertonstudents.com>
2024-12-04 23:48:37 +0100

    1-print_binary.c

--- bit_manipulation/main.c
+++ bit_manipulation/main.c
@@ -1,9 +1,21 @@
 #include "main.h"
 
+/**
+ * main - Check the code for printing binary representation
+ * Return: Always 0.
+ */
 int main(void)
 {
-unsigned long int number = 10;
-print_binary(number);
+print_binary(0);
 _putchar('\n');
+print_binary(1);
+_putchar('\n');
+print_binary(98);
+_putchar('\n');
+print_binary(1024);
+_putchar('\n');
+print_binary((1 << 10) + 1);
+_putchar('\n');
+
 return (0);
 }
