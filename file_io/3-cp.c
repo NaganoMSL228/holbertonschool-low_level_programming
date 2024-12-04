2d1025236c482bbe72e1ce26bf9c916df18d274b
NaganoMSL228 <9794@holbertonstudents.com>
2024-12-05 00:27:34 +0100

    Change the for 16 pts

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,48 +1,74 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-/**
-* read_textfile - Reads a text file and prints it to POSIX standard output.
-* @filename: The name of the file to read.
-* @letters: The number of letters to read and print.
-*
-* Return: The actual number of letters it could read and print, or 0 on failure
-*/
-ssize_t read_textfile(const char *filename, size_t letters)
+#define BUFFER_SIZE 1024
+#define PERMISSIONS 0664
+
+void print_error(const char *message, const char *filename)
 {
-	int fd;
+dprintf(STDERR_FILENO, message, filename);
+}
 
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+int main(int argc, char *argv[])
+{
+int src_fd, dest_fd;
+char buffer[BUFFER_SIZE];
+ssize_t bytes_read, bytes_written;
 
-	if (filename == NULL)
-		return (0);
+if (argc != 3)
+{
+print_error("Usage: cp %s\n", "file_from file_to");
+exit(97);
+}
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
+src_fd = open(argv[1], O_RDONLY);
+if (src_fd == -1)
+{
+ print_error("Error: Can't read from file %s\n", argv[1]);
+ exit(98);
+}
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+if (dest_fd == -1)
+{
+print_error("Error: Can't write to %s\n", argv[2]);
+close(src_fd);
+exit(99);
+}
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0)
+{
+bytes_written = write(dest_fd, buffer, bytes_read);
+if (bytes_written != bytes_read)
+{
+print_error("Error: Can't write to %s\n", argv[2]);
+close(src_fd);
+close(dest_fd);
+exit(99);
+}
+}
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	free(buffer);
-	close(fd);
+if (bytes_read == -1)
+{
+print_error("Error: Can't read from file %s\n", argv[1]);
+close(src_fd);
+close(dest_fd);
+exit(98);
+}
 
-	if (bytes_written != bytes_read)
-		return (0);
+if (close(src_fd) == -1)
+{
+print_error("Error: Can't close fd %d\n", src_fd);
+exit(100);
+}
+    
+if (close(dest_fd) == -1)
+{
+print_error("Error: Can't close fd %d\n", dest_fd);
+exit(100);
+}
 
-	return (bytes_written);
+return (0);
 }
